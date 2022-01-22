9b5ab49bbd14a523b8ff3a05cc33dd7da39e8b15
mert kiziloglu <mert_besiktas24@hotmail.com>
2022-01-22 22:53:42 +0300

    test

--- ps-Java/conference/src/main/java/com/example/conference/ConferenceApplication.java
+++ ps-Java/conference/src/main/java/com/example/conference/ConferenceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConferenceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConferenceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceApplication.class, args);
+    }
 
 }
